c0c666b59ff0a3db6d2329557119d164e1f8bb52
Ali Lozano <alilozanoc@gmail.com>
2023-04-29 10:42:14 -0500

    Agregada la búsqueda avanzada por titulo, autor y genero.

--- src/main/java/pe/edu/pe/onlinebooks/controllers/BookController.java
+++ src/main/java/pe/edu/pe/onlinebooks/controllers/BookController.java
@@ -1,5 +1,7 @@
 package pe.edu.pe.onlinebooks.controllers;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -22,6 +24,18 @@ public class BookController {
                                         @RequestParam String description,
                                         @RequestParam MultipartFile image) {
         Book newBook = bookService.addBook(title, author, description, image);
+        return ResponseEntity.ok(newBook);
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<Page<Book>> advanceSearch(@RequestParam(defaultValue = "", required = false) String title,
+                                                    @RequestParam(defaultValue = "", required = false) String author,
+                                                    @RequestParam(required = false) String genre,
+                                                    @RequestParam(defaultValue = "0", required = false) Integer page,
+                                                    @RequestParam(defaultValue = "10", required = false) Integer pageSize) {
+        Page<Book> newBook = bookService.advanceSearch(title, author, genre,
+                PageRequest.of(page, pageSize)
+        );
         return ResponseEntity.ok(newBook);
     }
 }
--- src/main/java/pe/edu/pe/onlinebooks/repositories/BookRepository.java
+++ src/main/java/pe/edu/pe/onlinebooks/repositories/BookRepository.java
@@ -1,12 +1,18 @@
 package pe.edu.pe.onlinebooks.repositories;
 
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import pe.edu.pe.onlinebooks.models.Book;
 
-import java.util.List;
-
 public interface BookRepository extends CrudRepository<Book, Integer> {
 
+    @Query("SELECT b from Book b where " +
+            "b.title like CONCAT('%', :title, '%') AND " +
+            "b.author like CONCAT('%', :author, '%') AND " +
+            "(:genre is null or :genre = '' or b.genre = :genre)"
+    )
+    Page<Book> advanceSearch(String title, String author, String genre, PageRequest page);
 }
